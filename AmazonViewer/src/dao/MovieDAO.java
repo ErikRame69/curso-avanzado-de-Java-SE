af46e46d49988d754c01ced10f35573b21a5f13a
Erik Daniel <erikrame69@gmail.com>
2021-07-19 13:31:08 -0500

    Añadimos el archivo MySQL.jar

--- AmazonViewer/src/dao/MovieDAO.java
+++ AmazonViewer/src/dao/MovieDAO.java
@@ -1,5 +1,24 @@
 package dao;
 
-public interface MovieDAO {
+import java.util.ArrayList;
 
+import com.anncode.amazonviewer.model.Movie;
+
+public interface MovieDAO {
+	
+	default Movie setMovieViewed()Movie movie {
+		return movie;
+	}
+	
+	default ArrayList<Movie> read(){
+		ArrayList<Movie> movies = new ArrayList<>();
+		return movie;
+	}
+	
+	private boolean getMovieViewed (){
+		
+		return false; 
+		
+	}
+	
 }
